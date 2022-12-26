49bc874743d6d5622dacca5ff5b73e5b1de787ef
topes1 <l52792@alunos.uevora.pt>
2022-12-26 18:33:04 +0000

    Worked on the product classes.

--- src/Product.java
+++ src/Product.java
@@ -1,5 +1,39 @@
 abstract class Product {
     String name;
     float value;
+    float getvalue(){
+        return value;
+    }
     abstract boolean is_persihable();
+}
+
+class Perishable extends Product {
+    float[] date;
+    String name;
+    Perishable(String name,float[] date,float value) {
+        this.name = name;
+        this.date = date;
+        this.value = value;
+    }
+    @Override
+    boolean is_persihable() {
+        return true;
+    }
+}
+
+class NonPerishable extends Product{
+    float volume;
+    float getvolume(){
+        return volume;
+    }
+
+    NonPerishable(String name,float value,float volume){
+        this.name = name;
+        this.value = value;
+        this.volume = volume;
+    }
+    @Override
+    boolean is_persihable() {
+        return false;
+    }
 }
