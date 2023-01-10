fe0d61a86f903f820846655d1fbeb814932c05ab
Th4t-guy <106694741+Th4t-guy@users.noreply.github.com>
2023-01-10 18:51:48 +0000

    elementarmachine

--- src/ElementarMachine.java
+++ src/ElementarMachine.java
@@ -1,18 +1,47 @@
 import java.util.ArrayList;
 
+
 public class ElementarMachine<T> {
-    ArrayList<T> elements;
+
+    ArrayList<Element<T>> elements;
+
 
     public ElementarMachine(){
-        elements = new ArrayList<>();
+        elements = new ArrayList<Element<T>>();
     }
 
 
-    public void addThings(int n, T){
+    public void addThings(int n, T thing){
+
+
+        for (int i = 0; i < elements.size(); i++) {
+            if(elements.get(i).getThing() == thing){
+                int x = elements.get(i).getCount();
+                elements.get(i).setCount(x+n);
+                return;
+            }
+        }
+
+        Element x = new Element<T>(n, thing);
+        x.setCount(n);
+        elements.add(x);
 
     }
-    public boolean removeOneThing(T){
+    public boolean removeOneThing(T thing){
 
+        for (int i = 0; i < elements.size(); i++) {
+            if(elements.get(i).getThing() == thing){
+                int x = elements.get(i).getCount();
+                elements.get(i).setCount(x-1);
+                return true;
+            }
+        }
+        return false;
     }
-    public void listAll(){}
+    public void listAll(){
+        for (Element<T> element :
+             elements) {
+            System.out.println(element.toString());
+        }
+    }
 }
